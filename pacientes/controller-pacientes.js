53814c45ceaafdceb854aa61f9dc7a04d5c7f33d
Mauricio <mromauricio@gmail.com>
2020-09-28 15:33:35 -0300

    fix #21 http status code adjustment

--- pacientes/controller-pacientes.js
+++ pacientes/controller-pacientes.js
@@ -16,60 +16,60 @@ router.post('/', async (req, res, next) => {
   switch (await servicePacientes.RuleInsertPaciente(req.body)) {
     case 0:
         return res.status(201).send();
-    case 1:
-        return res.status(500).send();
-    case 2:
+    case 3:
         return res.status(406).send();
+    case 5:
+      return res.status(500).send();    
   }
 });
 
 router.get('/nome', async (req, res, next) => {
   let retorno = await servicePacientes.RuleSelectPacienteNome(req.query.nome);
   switch (retorno) {  
-    case 1:
-        return res.status(500).send({'':''});
     case 2:
-        return res.status(406).send({'':''});
+      return res.status(404).send({'':''});    
     case 3:
-        return res.status(404).send({"Nome buscado não existe no Banco de Dados.":""});    
+      return res.status(406).send({'':''});
+    case 5:
+      return res.status(500).send({'':''});
     default:
-        return res.status(200).send(retorno);    
+      return res.status(200).send(retorno);    
   }
 });
 
 router.get('/cpf', async (req, res, next) => {
   let retorno = await servicePacientes.RuleSelectPacienteCpf(req.query.cpf);
   switch (retorno) {  
-    case 1:
+    case 2:
+      return res.status(404).send({'':''});    
+    case 5:
       return res.status(500).send({'':''});
-    case 3:
-        return res.status(404).send({"CPF buscado não existe no Banco de Dados.":""});    
     default:
-        return res.status(200).send(retorno);    
+      return res.status(200).send(retorno);    
   }
 });
 
 router.get('/cns', async (req, res, next) => {
   let retorno = await servicePacientes.RuleSelectPacienteCns(req.query.cns);
   switch (retorno) {  
-    case 1:
-        return res.status(500).send({'':''});
-    case 3:
-        return res.status(404).send({"CNS buscado não existe no Banco de Dados.":""});    
+    case 2:
+      return res.status(404).send({'':''});    
+    case 5:
+      return res.status(500).send({'':''});
     default:
-        return res.status(200).send(retorno);    
+      return res.status(200).send(retorno);    
   }
 });
 
 router.get('/registro', async (req, res, next) => {
   let retorno = await servicePacientes.RuleSelectPacienteRegistro(req.query.registro);
   switch (retorno) {  
-    case 1:
-        return res.status(500).send({'':''});
-    case 3:
-        return res.status(404).send({"Registro buscado não existe no Banco de Dados.":""});    
+    case 2:
+      return res.status(404).send({'':''});    
+    case 5:
+      return res.status(500).send({'':''});
     default:
-        return res.status(200).send(retorno);    
+      return res.status(200).send(retorno);    
   }
 });
 
@@ -78,10 +78,12 @@ router.put('/:id', async (req, res, next) => {
   switch (retorno) {
     case 0:
         return res.status(200).send();
-    case 1:
-        return res.status(400).send();
     case 2:
+        return res.status(404).send();
+    case 3:
         return res.status(406).send();
+    case 5:
+      return res.status(500).send();    
   }
 });
 
--- pacientes/service-pacientes.js
+++ pacientes/service-pacientes.js
@@ -10,23 +10,23 @@ exports.RuleInsertPaciente = async (data) => {
   data.nascimento = `'${dataTemp[2]}-${dataTemp[0]}-${dataTemp[1]}'`;
   }
   else data.nascimento = null;
-  if (data.cpf==data.cpfresp) return 2; // Regra que obriga CPF ser diferente do CPF resp
+  if (data.cpf==data.cpfresp) return 3; // Regra que obriga CPF ser diferente do CPF resp
   if (await daoPacientes.InsertPaciente(data) == 0) return 0;
-  return 1;
+  return 5;
 }
 
 exports.RuleSelectPacienteNome = async (query) => {
   query = query.toUpperCase();
-  if (query == '') return 2;   // Teste fictício de validação da regra de negócio para provocar return 2 
+  if (query == '') return 3;   // Teste fictício de validação da regra de negócio para provocar return 2 
   let retorno = await daoPacientes.SelectPacienteNome(query)
-  if (retorno == '') return 3;
-  if (retorno == 1) return 1;
+  if (retorno == '') return 2;
+  if (retorno == 5) return 5;
   return retorno;
 }
 
 exports.RuleUpdatePaciente = async (idSearch,data) => {
   idSearch = idSearch.replace('id','');
-  if (data.cpf==data.cpfresp) return 2; // Regra que obriga CPF ser diferente do CPF resp
+  if (data.cpf==data.cpfresp) return 3; // Regra que obriga CPF ser diferente do CPF resp
   data.nome = data.nome.toUpperCase();
   (data.menor)? data.menor = 1 : data.menor = 0;
   (data.whatsapp)? data.whatsapp = 1 : data.whatsapp = 0;
@@ -40,30 +40,28 @@ exports.RuleUpdatePaciente = async (idSearch,data) => {
   else data.nascimento = null;
   let retorno = await daoPacientes.UpdatePaciente(idSearch,data);
   if (retorno == 0) return 0;
-  return 1
+  if (retorno == 2) return 2;
+  return 5
 }
 
 
 exports.RuleSelectPacienteCpf = async (query) => {
-  if (query=='000') return 2;   // Teste fictício de validação da regra de negócio para provocar return 2 
   let retorno = await daoPacientes.SelectPacienteCpf(query)
-  if (retorno=='') return 3;
-  if (retorno==1) return 1;
+  if (retorno=='') return 2;
+  if (retorno==5) return 5;
   return retorno;
 }
 
 exports.RuleSelectPacienteCns = async (query) => {
-  if (query=='000.0000.0000.0000') return 2;   // Teste fictício de validação da regra de negócio para provocar return 2 
   let retorno = await daoPacientes.SelectPacienteCns(query)
-  if (retorno=='') return 3;
-  if (retorno==1) return 1;
+  if (retorno=='') return 2;
+  if (retorno==5) return 5;
   return retorno;
 }
 
 exports.RuleSelectPacienteRegistro = async (query) => {
-  if (query=='0.000.000') return 2;   // Teste fictício de validação da regra de negócio para provocar return 2 
   let retorno = await daoPacientes.SelectPacienteRegistro(query)
-  if (retorno=='') return 3;
-  if (retorno==1) return 1;
+  if (retorno=='') return 2;
+  if (retorno==5) return 5;
   return retorno;
 }

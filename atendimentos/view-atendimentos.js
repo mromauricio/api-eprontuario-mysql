3c31512339ed2ba9b922808d6324c8968e330ed8
Mauricio <mromauricio@gmail.com>
2020-10-10 12:05:35 -0300

    fixed path html

--- atendimentos/view-atendimentos.js
+++ atendimentos/view-atendimentos.js
@@ -3,16 +3,8 @@ const fs = require('fs'); // ou require('fs').promises;
 
 exports.ReadHtmlFile = async (htmlSearch) => {
   let retorno;
-  if (htmlSearch == 'atendimentos'){
    try{ 
-   retorno = await fs.promises.readFile('atendimentos/view-atendimentos.html');
+   retorno = await fs.promises.readFile(htmlSearch);
    } catch (err) { console.log(err); retorno = 2;}
-  }
-  else if (htmlSearch == 'busca'){
-    try{ 
-      retorno = await fs.promises.readFile('atendimentos/view-search.html');
-      } catch (err) { console.log(err); retorno = 2;}
-  }
-  else return 2;
   return retorno; 
 }

37328def675d323888f8a874081ba9a15d4c24b8
Mauricio <mromauricio@gmail.com>
2020-10-17 06:26:46 -0300

    SQL injection protection

--- pacientes/dao-pacientes.js
+++ pacientes/dao-pacientes.js
@@ -30,16 +30,14 @@ exports.InsertPaciente = async (data) => {
 
 exports.SelectPacienteNome = async (nome) => {
   let query = 'SELECT * FROM pacientes WHERE nome LIKE ? ORDER BY nome';
-  let post = nome;
   try{
-  let rows = await connecttion.query(query, post);
+  let rows = await connecttion.query(query, nome);
   return rows;
   } catch (err) { console.log(err); return 5;  }
 }
 
 exports.UpdatePaciente = async (idSearch, data) => {
   let query = 'UPDATE pacientes SET ? WHERE id_paciente = ?';
-  // let post = {nome=data.nome, menor=data.menor, responsavel=data.responsavel, cpfresp=data.cpfresp, cpf=data.cpf, cns=data.cns, registro=data.registro, nacionalidade=data.nacionalidade, nascimento=data.nascimento, genero=data.genero, tel=data.tel, cel=data.cel, whatsapp=data.whatsapp, email=data.email, endereco=data.endereco, cep=data.cep, bairro=data.bairro, uf=data.uf, cidade=data.cidade, historico=data.historico, medicamento=data.medicamento, cirurgia=data.cirurgia, trauma=data.trauma, ativo=data.ativo};
   let post = {nome:data.nome, menor:data.menor, responsavel:data.responsavel, cpfresp:data.cpfresp, cpf:data.cpf, cns:data.cns, registro:data.registro, nacionalidade:data.nacionalidade, nascimento:data.nascimento, genero:data.genero, tel:data.tel, cel:data.cel, whatsapp:data.whatsapp, email:data.email, endereco:data.endereco, cep:data.cep, bairro:data.bairro, uf:data.uf, cidade:data.cidade, historico:data.historico, medicamento:data.medicamento, cirurgia:data.cirurgia, trauma:data.trauma, ativo:data.ativo};
   try{
       let retorno = await connecttion.query(query, [post, idSearch]);
@@ -50,9 +48,8 @@ exports.UpdatePaciente = async (idSearch, data) => {
 
 exports.UpdatePacienteDatalog = async (idSearch) => {
   let query = 'UPDATE pacientes SET datalog=current_timestamp WHERE id_paciente = ?';
-  let post = idSearch;
   try{
-      let retorno = await connecttion.query(query, post);
+      let retorno = await connecttion.query(query, idSearch);
       if (retorno.affectedRows == 1) return 0;
       return 2
   } catch (err) { console.log(err); return 5;  }
@@ -60,36 +57,32 @@ exports.UpdatePacienteDatalog = async (idSearch) => {
 
 exports.SelectPacienteCpf = async (cpf) => {
   let query = 'SELECT * FROM pacientes WHERE cpf LIKE ?';
-  let post = cpf;
   try{
-  let rows = await connecttion.query(query, post);
+  let rows = await connecttion.query(query, cpf);
   return rows;
   } catch (err) { console.log(err); return 5;  }
 }
 
 exports.SelectPacienteCpfResp = async (cpfresp) => {
   let query = 'SELECT * FROM pacientes WHERE cpfresp LIKE ?';
-  let post = cpfresp;
   try{
-  let rows = await connecttion.query(query, post);
+  let rows = await connecttion.query(query, cpfresp);
   return rows;
   } catch (err) { console.log(err); return 5;  }
 }
 
 exports.SelectPacienteCns = async (cns) => {
   let query = 'SELECT * FROM pacientes WHERE cns LIKE ?';
-  let post = cns;
   try{
-  let rows = await connecttion.query(query, post);
+  let rows = await connecttion.query(query, cns);
   return rows;
   } catch (err) { console.log(err); return 5;  }
 }  
 
 exports.SelectPacienteRegistro = async (registro) => {
   let query = 'SELECT * FROM pacientes WHERE registro LIKE ?';
-  let post = registro;
   try{
-  let rows = await connecttion.query(query, post);
+  let rows = await connecttion.query(query, registro);
   return rows;
   } catch (err) { console.log(err); return 5;  }
 }  

d3d89dde71c32ff1e39fe37cda02ddd7072e4656
Mauricio <mromauricio@gmail.com>
2020-10-25 15:35:51 -0300

    Refactoring Atendimentos

--- atendimentos/dao-atendimentos.js
+++ atendimentos/dao-atendimentos.js
@@ -20,10 +20,16 @@ exports.InsertTratamento = async (data) => {
 }
 
 exports.InsertAtendimento = async (data) => {
-  let query = 'INSERT INTO atendimentos SET ?';
-  let post = {id_atendimento:0, id_profissional:data.id_profissional, id_tratamento:data.id_tratamento, data:data.data, horario:data.horario, duracao:data.duracao, queixa:data.queixa, quadrogeral:data.quadrogeral, trajetodor:data.trajetodor, intensidadedor:data.intensidadedor, tipodor:data.tipodor, evolucao:data.evolucao, agravante:data.agravante, atenuante:data.atenuante, tratamentoanterior:data.tratamentoanterior};
+  let query1 = 'UPDATE tratamentos SET ? WHERE id_tratamento = ?';
+  let post1 = {descricao:data.titulotratamento, status:data.status};
   try{
-    let retorno =  await connecttion.query(query, post);
+      await connecttion.query(query1, [post1, data.id_tratamento]);
+  }
+  catch (err) { console.log(err); return 5; }
+  let query2 = 'INSERT INTO atendimentos SET ?';
+  let post2 = {id_atendimento:0, id_profissional:data.id_profissional, id_tratamento:data.id_tratamento, data:data.data, horario:data.horario, duracao:data.duracao, queixa:data.queixa, quadrogeral:data.quadrogeral, trajetodor:data.trajetodor, intensidadedor:data.intensidadedor, tipodor:data.tipodor, evolucao:data.evolucao, agravante:data.agravante, atenuante:data.atenuante, tratamentoanterior:data.tratamentoanterior};
+  try{
+    let retorno =  await connecttion.query(query2, post2);
     if (retorno.affectedRows == 1 ) return 0;
     return 5;
   }
@@ -42,7 +48,7 @@ exports.UpdateAtendimento = async (data) => {
 }
 
 exports.SelectTratamentosPaciente = async (id_paciente) => {
-  let query = 'SELECT * FROM tratamentos WHERE id_paciente=? ORDER BY datalog desc';
+  let query = 'SELECT * FROM tratamentos WHERE id_paciente=? ORDER BY datalog DESC';
   try{
   let rows = await connecttion.query(query, id_paciente);
   return rows;
@@ -50,7 +56,7 @@ exports.SelectTratamentosPaciente = async (id_paciente) => {
 }
 
 exports.SelectAtendimentosTratamento = async (id_tratamento) => {
-  let query = 'SELECT pro.nome profissional, pac.nome paciente, t.descricao titulotratamento, t.status, t.id_paciente, a.* FROM atendimentos a INNER JOIN profissionais pro ON a.id_profissional=pro.id_profissional  INNER JOIN tratamentos t ON a.id_tratamento=t.id_tratamento  INNER JOIN pacientes pac ON pac.id_paciente=t.id_paciente WHERE  a.id_tratamento=?';
+  let query = 'SELECT pro.nome profissional, pac.nome paciente, t.descricao titulotratamento, t.status, t.id_paciente, a.* FROM atendimentos a INNER JOIN profissionais pro ON a.id_profissional=pro.id_profissional  INNER JOIN tratamentos t ON a.id_tratamento=t.id_tratamento  INNER JOIN pacientes pac ON pac.id_paciente=t.id_paciente WHERE  a.id_tratamento=? ORDER BY a.data DESC';
   try{
   let rows = await connecttion.query(query, id_tratamento);
   return rows;

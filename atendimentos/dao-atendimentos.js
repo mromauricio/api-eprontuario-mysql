3143c7db66034fed1c99743d9ba39d863a885635
Mauricio <mromauricio@gmail.com>
2020-11-03 17:44:50 -0300

    New GetPaciente

--- atendimentos/dao-atendimentos.js
+++ atendimentos/dao-atendimentos.js
@@ -10,7 +10,7 @@ exports.InsertTratamento = async (data) => {
   }
   catch (err) { console.log(err); return 5; }
   let query2 = 'INSERT INTO atendimentos SET ?';
-  let post2 = {id_atendimento:0, id_tratamento:retornoInsert.insertId, id_profissional:data.id_profissional, preenchido:data.preenchido, data:data.data, horario:data.horario, duracao:data.duracao, queixa:data.queixa, quadrogeral:data.quadrogeral, trajetodor:data.trajetodor, intensidadedor:data.intensidadedor, tipodor:data.tipodor, avaliacao:data.avaliacao, agravante:data.agravante, atenuante:data.atenuante, tratamentoanterior:data.tratamentoanterior, id_formulario:data.id_formulario};
+  let post2 = {id_atendimento:0, id_tratamento:retornoInsert.insertId, id_profissional:data.id_profissional, preenchido:data.preenchido, data:data.data, horario:data.horario, duracao:data.duracao, queixa:data.queixa, quadrogeral:data.quadrogeral, trajetodor:data.trajetodor, intensidadedor:data.intensidadedor, tipodor:data.tipodor, avaliacao:data.avaliacao, agravante:data.agravante, atenuante:data.atenuante, tratamentoanterior:data.tratamentoanterior, id_formulario:data.id_formulario, faixaetaria:data.faixaetaria};
   try{
     let retorno =  await connecttion.query(query2, post2);
     if (retorno.affectedRows == 1 ) return 0;
@@ -27,7 +27,7 @@ exports.InsertAtendimento = async (data) => {
   }
   catch (err) { console.log(err); return 5; }
   let query2 = 'INSERT INTO atendimentos SET ?';
-  let post2 = {id_atendimento:0, id_profissional:data.id_profissional, id_tratamento:data.id_tratamento, preenchido:data.preenchido, data:data.data, horario:data.horario, duracao:data.duracao, queixa:data.queixa, quadrogeral:data.quadrogeral, trajetodor:data.trajetodor, intensidadedor:data.intensidadedor, tipodor:data.tipodor, avaliacao:data.avaliacao, agravante:data.agravante, atenuante:data.atenuante, tratamentoanterior:data.tratamentoanterior, id_formulario:data.id_formulario};
+  let post2 = {id_atendimento:0, id_profissional:data.id_profissional, id_tratamento:data.id_tratamento, preenchido:data.preenchido, data:data.data, horario:data.horario, duracao:data.duracao, queixa:data.queixa, quadrogeral:data.quadrogeral, trajetodor:data.trajetodor, intensidadedor:data.intensidadedor, tipodor:data.tipodor, avaliacao:data.avaliacao, agravante:data.agravante, atenuante:data.atenuante, tratamentoanterior:data.tratamentoanterior, id_formulario:data.id_formulario, faixaetaria:data.faixaetaria};
   try{
     let retorno =  await connecttion.query(query2, post2);
     if (retorno.affectedRows == 1 ) return 0;
